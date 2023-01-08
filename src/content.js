e79db115697373b3679c32abd247ed724375c8e6
Malik Elate <Malik.e1955@gmail.com>
2023-01-08 14:17:24 -0600

    this is the first commmit of the mvp

--- src/content.js
+++ src/content.js
@@ -1,15 +1,39 @@
 import * as InboxSDK from '@inboxsdk/core';
 
-InboxSDK.load(2, "Hello World!").then((sdk) => {
+InboxSDK.load(2, "sdk_ez-write-mv3_a2faec26d6").then((sdk) => {
   // the SDK has been loaded, now do something with it!
   sdk.Compose.registerComposeViewHandler((composeView) => {
     // a compose view has come into existence, do something with it!
+    console.log("compose view: ", composeView)
+    console.log("compose view with get html: ", composeView.getHTMLContent())
     composeView.addButton({
-      title: "My Nifty Button!",
+      title: "EZ Write",
       iconUrl:
-        "https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365",
+      "https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365",
       onClick(event) {
-        event.composeView.insertTextIntoBodyAtCursor("Hello World!");
+        let emailPrompt = composeView.getTextContent(); 
+        let newEmail; 
+        fetch('https://api.openai.com/v1/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 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'
+        },
+        body: JSON.stringify({
+            'model': 'text-davinci-003',
+            'prompt': `${emailPrompt}`,
+            'max_tokens': 500,
+            'top_p': 1,
+            'frequency_penalty': 0,
+            'presence_penalty': 0
+        })
+      }).then(response => response.json())
+        .then(data => {
+          console.log("data.choices[0].text", data.choices[0].text);
+          console.log("data.choices[0]", data.choices[0]);
+          newEmail = data.choices[0].text; 
+          event.composeView.setBodyText(newEmail); 
+      })
       },
     });
   });
